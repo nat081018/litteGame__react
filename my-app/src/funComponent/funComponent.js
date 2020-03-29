037b6917a3c92e17f302d6ce49b47f4454c1c533
Nataly Makarova <natmaklis@gmail.com>
2020-03-29 15:11:02 +0300

    create

---
+++ my-app/src/funComponent/funComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./funComponent.css";
+
+const Greetings = props => {
+    <p>  использование без класса {props.first} {props.last}<p>
+
+};
+
+{/* export default Greetings */}
+
